8824e57fb5d67c1617e189c99daec98fcbe9e7f7
hbiarge <hugo@arosbi.com>
2012-06-06 22:40:04 +0200

    Añadidos archivos principales de la aplicación
    Global.asax
    index.html
    Race.cs -> Hub de signalR con las operaciones disponibles en cliente
    RaceManager.cs -> Gestiona el juego

---
+++ MathRace/MathRace/Race.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using SignalR.Hubs;
+
+namespace MathRace
+{
+    public class Race : Hub, IConnected
+    {
+        public Task Connect()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Reconnect(IEnumerable<string> groups)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MathRace/MathRace/RaceManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MathRace
+{
+    public static class RaceManager
+    {
+        public static void Start()
+        {
+
+        }
+    }
+}
